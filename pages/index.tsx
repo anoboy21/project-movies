4b41f81be3a79ccded4d092f3170ac3fe945550e
HowardLight <howardlight+github@protonmail.com>
2022-10-15 00:01:23 +0300

    Removed boilerplate

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
-import type { NextPage } from 'next'
+import type { GetStaticProps, NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import useSWR from 'swr';
 import styles from '../styles/Home.module.css'
 
-const Home: NextPage = () => {
+
+export const fetcher = (...args: any[]) => fetch(...args).then(res => res.json());
+
+const Home: NextPage = (body) => {
+
+  const { data, error } = useSWR('/api/getpopular/1', fetcher);
+  console.log(data);
   return (
     <div className={styles.container}>
       <Head>
@@ -12,61 +19,25 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <main className={styles.main}>
+          <h1>Project Movies</h1>
+        </main>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
 
-export default Home
+// export const getStaticProps: GetStaticProps = async () => {
+
+//   var body;
+//   const request = await fetch("https://api.themoviedb.org/3/movie/550?api_key=be2b13195090224a6edfaa7360f1cf54");
+//   body = await request.json();
+
+//   return {
+//     props: {
+//       body: body
+//     }
+//   }
+// }
+
+export default Home;
