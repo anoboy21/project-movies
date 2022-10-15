2772bf178f25908bce2707d0eae9154c4d8c9c02
HowardLight <howardlight+github@protonmail.com>
2022-10-15 10:20:05 +0300

    Fixed rookie mistake

--- types/GetPopularTypes.ts
+++ types/GetPopularTypes.ts
@@ -1,25 +1,25 @@
 export interface PopularResponse {
-    page:         number;
-    results:      Result[];
-    totalResults: number;
-    totalPages:   number;
+    page:          number;
+    results:       Result[];
+    total_results: number;
+    total_pages:   number;
 }
 
 export interface Result {
-    posterPath:       string;
-    adult:            boolean;
-    overview:         string;
-    releaseDate:      Date;
-    genreIDS:         number[];
-    id:               number;
-    originalTitle:    string;
-    originalLanguage: OriginalLanguage;
-    title:            string;
-    backdropPath:     string;
-    popularity:       number;
-    voteCount:        number;
-    video:            boolean;
-    voteAverage:      number;
+    poster_path:       string;
+    adult:             boolean;
+    overview:          string;
+    release_date:      Date;
+    genre_ids:         number[];
+    id:                number;
+    original_title:    string;
+    original_language: OriginalLanguage;
+    title:             string;
+    backdrop_path:     string;
+    popularity:        number;
+    vote_count:        number;
+    video:             boolean;
+    vote_average:      number;
 }
 
 export enum OriginalLanguage {
