ba97bf11e57413e95604f18cfe5b8c87c4a1cf7d
HowardLight <howardlight+github@protonmail.com>
2022-10-15 10:20:57 +0300

    Updated index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,18 @@
 import type { GetStaticProps, NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import useSWR from 'swr';
+import { Fragment } from 'react';
+import useSWR, { SWRResponse } from 'swr';
+import { PosterLoader } from '../PosterLoader';
 import styles from '../styles/Home.module.css'
+import { PopularResponse, Result } from '../types/getPopularTypes';
 
 // @ts-ignore
 export const fetcher = (...args: any[]) => fetch(...args).then(res => res.json());
 
 const Home: NextPage = (body) => {
 
-  const { data, error } = useSWR('/api/getpopular/1', fetcher);
+  const { data, error }: SWRResponse<PopularResponse, Error> = useSWR('/api/getpopular/1', fetcher);
   console.log(data);
   return (
     <div className={styles.container}>
@@ -19,25 +22,35 @@ const Home: NextPage = (body) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-        <main className={styles.main}>
-          <h1>Project Movies</h1>
-        </main>
+      <main className={styles.main}>
+        <h1>Project Movies</h1>
+        {!data && !error ?
+          <p>Loading...</p> :
+          !data ? <p>An error occured</p> :
+            getPopular(data)
+        }
+      </main>
 
     </div>
   )
 }
 
-// export const getStaticProps: GetStaticProps = async () => {
-
-//   var body;
-//   const request = await fetch("https://api.themoviedb.org/3/movie/550?api_key=be2b13195090224a6edfaa7360f1cf54");
-//   body = await request.json();
-
-//   return {
-//     props: {
-//       body: body
-//     }
-//   }
-// }
+const getPopular = (popularResponse: PopularResponse): React.ReactElement => {
+  return (
+    <div className='grid grid-rows-3 grid-flow-col'>
+      {popularResponse.results.map((item: Result, index: number) => (
+        <div key={item.id}>
+          <Image
+            src={item.poster_path}
+            loader={PosterLoader}
+            alt={`${item.title} poster`}
+            width={500}
+            height={500}
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default Home;
