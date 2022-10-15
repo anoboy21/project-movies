9f7863b0ea1221ca4142f04bdc203ab4bc347be7
HowardLight <howardlight+github@protonmail.com>
2022-10-15 10:19:56 +0300

    Created PosterLoader

---
+++ PosterLoader.ts
@@ -0,0 +1,12 @@
+
+//TODO: add a placeholder case
+//TODO: Figure out width handling
+/**
+ * 
+ * @param src - File name of the poster
+ * @param width - The desired Width. Currently unused, WIP
+ * @returns 
+ */
+export const PosterLoader = ({src, width}: {src: string, width: string}) => {
+    return `https://image.tmdb.org/t/p/w500/${src}`;
+}
