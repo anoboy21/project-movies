22ffa0a9c1051bc74c68d5409b824ed2fc6168d7
HowardLight <howardlight+github@protonmail.com>
2022-10-16 22:07:43 +0300

    Updated [id]

--- pages/movie/[id].tsx
+++ pages/movie/[id].tsx
@@ -8,39 +8,40 @@ import { PosterLoader } from "../../PosterLoader";
 export default function MoviePage({ data }: { data: Movie }) {
     console.log(data);
     return (
-        <div className="m-3">
-            <div className="flex flex-col justify-center items-center mb-16">
-                <Image
-                    src={data.poster_path ? data.poster_path : `https://via.placeholder.com/250x375?text=${data.title}`}
-                    loader={PosterLoader}
-                    alt={`${data.title} Poster`}
-                    width={250}
-                    height={375}
-                    className="rounded-md"
-                />
-                <div>
-                    <div className="flex flex-col grow mt-5">
-                        <p className="font-bold text-3xl self-center">{data.title}</p>
-                        <p className="text-sm self-center">{data.tagline}</p>
-                    </div>
-                    <div className="flex flex-row mt-5 gap-3 justify-center">
-                        {data.genres.map((genre) => (
-                            <div key={`genre-${genre.id}`} className="text-base font-medium bg-red-600 p-2 rounded-md">
-                                {genre.name}
-                            </div>
-                        ))}
+        <Fragment>
+            <div style={{backgroundImage: `linear-gradient(to right, rgba(24, 26, 27, 0.84), rgba(0,0,0, 0.8)), url(https://image.tmdb.org/t/p/original/${data.backdrop_path})`}}>
+                <div className="flex flex-col justify-center items-center mb-8 p-5">
+                    <Image
+                        src={data.poster_path ? data.poster_path : `https://via.placeholder.com/250x375?text=${data.title}`}
+                        loader={PosterLoader}
+                        alt={`${data.title} Poster`}
+                        width={250}
+                        height={375}
+                        className="rounded-md"
+                    />
+                    <div>
+                        <div className="flex flex-col grow mt-5">
+                            <p className="font-bold text-3xl self-center text-neutral-100">{data.title}</p>
+                            <p className="text-sm self-center text-neutral-300">{data.tagline}</p>
+                        </div>
+                        <div className="flex flex-row mt-5 gap-3 justify-center">
+                            {data.genres.map((genre) => (
+                                <div key={`genre-${genre.id}`} className="text-base text-neutral-300 font-medium bg-red-600 p-2 rounded-md">
+                                    {genre.name}
+                                </div>
+                            ))}
+                        </div>
                     </div>
                 </div>
             </div>
-            <div>
+            <div className="m-3">
                 <div>
-                    <p className="font-semibold text-3xl">Overview</p>
-                    <p>{data.overview}</p>
+                    <p className="font-semibold text-2xl text-neutral-100">Overview</p>
+                    <p className="text-neutral-300">{data.overview}</p>
                 </div>
-                <p>{data.video}</p>
             </div>
 
-        </div>
+        </Fragment>
     )
 }
 
