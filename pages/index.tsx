1e2d4f7c59818f7458db016f90a71739db098c43
HowardLight <howardlight+github@protonmail.com>
2022-10-16 14:23:56 +0300

    Updated index

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,8 @@ import { Fragment } from 'react';
 import useSWR, { SWRResponse } from 'swr';
 import { PosterLoader } from '../PosterLoader';
 import styles from '../styles/Home.module.css'
-import { PopularResponse, Result } from '../types/GetPopularTypes';
+import { PopularResponse, PopularResult } from '../types/GetPopularTypes';
+import { UpcomingResponse, UpcomingResult } from '../types/GetUpcomingTypes';
 
 // @ts-ignore
 export const fetcher = (...args: any[]) => fetch(...args).then(res => res.json());
@@ -22,6 +23,7 @@ const Home: NextPage = () => {
 
       <main>
         <PopularWidget />
+        <UpcomingWidget />
       </main>
 
     </div>
@@ -35,23 +37,62 @@ const PopularWidget = (): React.ReactElement => {
   if (error) return <p>An error occured.</p>
   return (
     <div className=''>
-      <h1 className='m-3 font-semibold text-2xl text-gray-100'>What&apos;s Popular</h1>
+      <h1 className='mt-3 ml-3 font-semibold text-2xl text-gray-900'>What&apos;s Popular</h1>
       <div className='grid grid-cols-2'>
-        {data!.results.map((item: Result, index: number) => (
-          <div key={item.id} className="m-3 bg-neutral-900 rounded-sm text-xsm ">
-            <Image
-              src={item.poster_path}
-              loader={PosterLoader}
-              alt={`${item.title} poster`}
-              width={500}
-              height={500}
-            />
-            <p className='font-medium text-lg ml-2 pb-2 text-gray-200'>{item.title}</p>
-          </div>
-        ))}
+        {data!.results.map((item: PopularResult, index: number) => {
+          if (index <= 3) return (
+            <div key={item.id} className="flex flex-col m-3 bg-neutral-100 shadow-md shadow-neutral-300 rounded-sm text-xsm">
+              <Image
+                src={item.poster_path}
+                loader={PosterLoader}
+                alt={`${item.title} poster`}
+                width={250}
+                height={250}
+              />
+              <div className='flex flex-col grow justify-between mt-2'>
+                <p className='font-medium text-lg ml-2 pb-2 text-gray-800'>{item.title}</p>
+                <p className='font-medium text-md ml-2 pb-2 text-gray-600 justify-end'>{(item.release_date).toString()}</p>
+              </div>
+            </div>
+          )
+          else return "";
+        })}
       </div>
     </div>
 
+  );
+}
+
+const UpcomingWidget = (): React.ReactElement => {
+
+  const { data, error }: SWRResponse<UpcomingResponse, Error> = useSWR("/api/getupcoming/1", fetcher);
+
+  if (!data && !error) return <p>Loading...</p>
+  if (error) return <p>An error occured</p>
+  return (
+    <Fragment>
+      <h1 className='mt-3 ml-3 font-semibold text-2xl text-gray-900'>Upcoming</h1>
+      <div className='grid grid-cols-2'>
+        {data!.results.map((item: UpcomingResult, index: number) => {
+          if (index <= 3) return (
+            <div key={item.id} className="flex flex-col m-3 bg-neutral-100 shadow-md shadow-neutral-300 rounded-sm text-xsm">
+              <Image
+                src={item.poster_path}
+                loader={PosterLoader}
+                alt={`${item.title} poster`}
+                width={250}
+                height={250}
+              />
+              <div className='flex flex-col grow justify-between mt-2'>
+                <p className='font-medium text-lg ml-2 pb-2 text-gray-800'>{item.title}</p>
+                <p className='font-medium text-md ml-2 pb-2 text-gray-600 justify-end'>`Releasing in {}`</p>
+              </div>
+            </div>
+          )
+          else return "";
+        })}
+      </div>
+    </Fragment>
   );
 }
 
