bbb9104848c3965549e7e5a579e1588932e39624
HowardLight <howardlight+github@protonmail.com>
2022-10-19 19:21:57 +0300

    Updated _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Fragment } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <Component {...pageProps} />
+    </Fragment>
+  )
+    ;
 }
 
 export default MyApp
