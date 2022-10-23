8ddef75cb22ccd4f3b63552246596faf2579de69
HowardLight <howardlight+github@protonmail.com>
2022-10-23 13:04:22 +0300

    Added Actors to Movie Page

--- pages/movie/[id].tsx
+++ pages/movie/[id].tsx
@@ -1,17 +1,21 @@
 import { GetServerSidePropsContext } from "next";
-import { Fragment } from "react";
-import useSWR from "swr";
+import useSWR, { SWRResponse } from "swr";
 import { Movie } from "../../types/Movie";
 import Image from "next/future/image";
 import { PosterLoader } from "../../PosterLoader";
 import { Navbar } from "../../components/Navbar";
+import moment from "moment";
+import { fetcher } from "..";
+import { CreditsResponse } from "../../types/GetCreditsTypes";
+import { Cast as CastContent } from "../../types/Cast";
+import Placeholder from "../../assets/MovieSVG.svg";
 
 export default function MoviePage({ data }: { data: Movie }) {
-    console.log(data);
+    // console.log(data);
     return (
-        <Fragment>
-            <div style={{backgroundImage: `linear-gradient(to right, rgba(24, 26, 27, 0.84), rgba(0,0,0, 0.8)), url(https://image.tmdb.org/t/p/original/${data.backdrop_path})`}}>
-            <Navbar />
+        <div>
+            <div style={{ backgroundImage: `linear-gradient(to right, rgba(24, 26, 27, 0.84), rgba(0,0,0, 0.8)), url(https://image.tmdb.org/t/p/original/${data.backdrop_path})` }}>
+                <Navbar />
                 <div className="flex flex-col justify-center items-center p-5">
                     <Image
                         src={data.poster_path ? data.poster_path : `https://via.placeholder.com/250x375?text=${data.title}`}
@@ -37,13 +41,88 @@ export default function MoviePage({ data }: { data: Movie }) {
                 </div>
             </div>
             <div className="m-3">
+
+                <Metrics data={data} styles="mb-5" />
                 <div>
+                    <p className="font-medium text-lg">Released on {moment(data.release_date).format("LL")}</p>
+                    <div className="text-lg font-medium">
+                        <p className="inline text-red-600">{data.runtime} Minutes</p>
+                        <p className="inline"> of runtime</p>
+                    </div>
+                </div>
+                <br />
+                <div className="">
                     <p className="font-semibold text-2xl text-neutral-100">Overview</p>
                     <p className="text-neutral-300">{data.overview}</p>
                 </div>
+                <br />
+
+                <CastWidget id={data.id} />
             </div>
 
-        </Fragment>
+        </div>
+    )
+}
+
+const Metrics = ({ data, styles }: { data: Movie, styles: string }) => {
+
+    const percentage = Math.round(data.vote_average * 10).toString();
+
+    return (
+        <div className={`${styles}`}>
+            <div className='h-4 w-full bg-neutral-900 rounded-sm flex items-center'>
+                <span className={`inline-block relative bg-red-600 h-2 ml-1 mr-2`} style={{ width: `${percentage}%` }}></span>
+            </div>
+            <div className="flex flex-row justify-between ml-1 mt-2 mr-1">
+                <p className='font-bold text-2xl text-red-600'>{percentage}%</p>
+                <div className="flex flex-row gap-1 items-center">
+                    <p className="font-semibold text-2xl text-red-600 inline">{data.vote_count}</p>
+                    <p className="font-medium inline text-red-600 text-xl">Reviews</p>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+const CastWidget = ({ id }: { id: number }) => {
+    return (
+        <div>
+            <p className="font-semibold text-2xl text-neutral-100 mb-3">Actors</p>
+            <CastContent id={id} />
+        </div>
+    )
+}
+
+const CastContent = ({ id }: { id: number }) => {
+
+    const { data, error }: SWRResponse<CreditsResponse, Error> = useSWR(`/api/getcredits/${id}`, fetcher);
+
+    console.log(data);
+
+    //TODO: Replace placeholder.com with something better
+    if (!data && !error) return <p>Loading...</p>;
+    if (!data) return <p>Error occured</p>;
+    return (
+        <div className="flex flex-row overflow-x-auto">
+            {data.cast.map((cast: CastContent, index: number) => {
+                if (index <= 20) return (<div key={cast.id} className="grid auto-cols-max ml-2 mr-2">
+                    <Image
+                        src={cast.profile_path ? cast.profile_path : Placeholder.src}
+                        alt={`Image of ${cast.name}`}
+                        loader={PosterLoader}
+                        width={125}
+                        height={187}
+                        className="rounded-md w-[125px] h-[187px]"
+                    />
+                    <div className="w-[125px] truncate overflow-x-hidden text-neutral-100">
+                        <p className="truncate">{cast.name}</p>
+                        <p className="truncate text-neutral-400">{cast.character}</p>
+                    </div>
+                </div>
+                );
+            })}
+        </div>
     )
 }
 
@@ -53,7 +132,6 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
     const request = await fetch(`https://api.themoviedb.org/3/movie/${context!.params!.id}?api_key=${process.env.TMDB_API_KEY}&language=en-US`);
     data = await request.json();
 
-    // console.log(data);
     return {
         props: {
             data: data
