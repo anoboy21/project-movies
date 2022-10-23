0a38062206c2d4f8e2fd449fce0c5c08e558b4fd
HowardLight <howardlight+github@protonmail.com>
2022-10-23 13:03:49 +0300

    Created getCredits API

---
+++ pages/api/getcredits/[...args].ts
@@ -0,0 +1,26 @@
+
+
+import { NextApiRequest, NextApiResponse } from "next";
+
+/**
+ *  test
+ * 
+ * Language is hardcoded en-US
+ * @param req - Page
+ * @param res 
+ */
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    // Returns a List, first element is the parent folder NAME, in this case: getpopular
+    const slugs = req.query;
+    const credit_id = slugs.args![0];
+    let data;
+
+    const request = await fetch(`https://api.themoviedb.org/3/movie/${credit_id}/credits?api_key=${process.env.TMDB_API_KEY}&language=en-US`);
+    data = await request.json();
+
+
+    if(request.status == 401) res.status(401).json(data);
+    else if(request.status == 404) res.status(404).json(data);
+    else if(request.status != 200) res.status(500).json({status_message: "internal server error, this should not appear."})
+    else res.status(200).json(data);
+}
