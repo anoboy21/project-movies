4390524dcba446fba9fecbea5043453967601db4
HowardLight <howardlight+github@protonmail.com>
2022-11-03 22:58:20 +0200

    Added shallow TVShow type

---
+++ types/TVShow.ts
@@ -0,0 +1,15 @@
+export interface TVShow {
+    backdrop_path:     null;
+    first_air_date:    Date;
+    genre_ids:         number[];
+    id:                number;
+    original_language: string;
+    original_name:     string;
+    overview:          string;
+    origin_country:    string[];
+    poster_path:       null;
+    popularity:        number;
+    name:              string;
+    vote_average:      number;
+    vote_count:        number;
+}
