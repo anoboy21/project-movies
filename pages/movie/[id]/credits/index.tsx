564bc78144280e8b2da9f9df6fb949a0ce87a2cd
HowardLight <howardlight+github@protonmail.com>
2022-11-06 13:51:01 +0200

    Updated Movie Credits Page

--- pages/movie/[id]/credits/index.tsx
+++ pages/movie/[id]/credits/index.tsx
@@ -1,14 +1,39 @@
-import { GetServerSideProps, GetServerSidePropsContext } from "next";
+import { Tab } from "@headlessui/react";
+import { GetServerSidePropsContext } from "next";
 import { Fragment } from "react";
+import { Navbar } from "../../../../components/Navbar";
+import { MultiSearchPersonCard } from "../../../../components/Search/MultiSearchCards";
 import { CreditsResponse } from "../../../../types/GetCreditsTypes";
 
-const MovieCredits = ({data}: {data: CreditsResponse}) => {
-    
-    console.log(data);
-    
-    return(
-        <Fragment>
+const MovieCredits = ({ data }: { data: CreditsResponse }) => {
 
+
+    //TODO: FINISH THIS LATER
+    //TODO: ADD PAGINATION SINCE CREW AND CAST CAN GET MASSIVE
+    //TODO: CREATE COMPONENTS TAILORED TO THIS PAGE OR ADAPT MULTISEARCHPERSON TO BE MODULAR
+    console.log(data);
+
+    return (
+        <Fragment>
+            <Navbar />
+            <Tab.Group>
+                <Tab.List className={"flex flex-row justify-around font-semibold text-3xl gap-2 mt-4 pb-2 border-b-4 border-red-500"}>
+                    <Tab className={"bg-red-500 rounded-sm pt-2 pb-2 grow ml-2 text-neutral-100"}>Cast</Tab>
+                    <Tab className={"bg-red-500 rounded-sm pt-2 pb-2 grow mr-2 text-neutral-100"}>Crew</Tab>
+                </Tab.List>
+                <Tab.Panels className={"ml-2 mr-2"}>
+                    <Tab.Panel>
+                        {data.cast.map((cast, index) => (
+                            <MultiSearchPersonCard key={cast.cast_id} result={cast} />
+                        ))}
+                    </Tab.Panel>
+                    <Tab.Panel>
+                        {data.crew.map((crew, index) => (
+                            <MultiSearchPersonCard key={crew.credit_id} result={crew} />
+                        ))}
+                    </Tab.Panel>
+                </Tab.Panels>
+            </Tab.Group>
         </Fragment>
     );
 }
