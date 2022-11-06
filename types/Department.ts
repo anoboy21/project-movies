e44346014be3f3d200ea873d89f2ff820eb32911
HowardLight <howardlight+github@protonmail.com>
2022-11-06 19:38:33 +0200

    Created Department Type

---
+++ types/Department.ts
@@ -0,0 +1,13 @@
+export enum Department {
+    Acting = "Acting",
+    Art = "Art",
+    Camera = "Camera",
+    CostumeMakeUp = "Costume & Make-Up",
+    Crew = "Crew",
+    Directing = "Directing",
+    Editing = "Editing",
+    Production = "Production",
+    Sound = "Sound",
+    VisualEffects = "Visual Effects",
+    Writing = "Writing",
+}
