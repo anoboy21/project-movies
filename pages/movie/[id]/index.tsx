6d71589793f418c66443fd87f2477ca2b1a8d228
HowardLight <howardlight+github@protonmail.com>
2022-11-06 10:41:24 +0200

    Updated File based routing

--- pages/movie/[id]/index.tsx
+++ pages/movie/[id]/index.tsx
@@ -1,21 +1,22 @@
 import { GetServerSidePropsContext } from "next";
 import useSWR, { SWRResponse } from "swr";
-import { Movie } from "../../types/Movie";
+import { Movie } from "../../../types/Movie";
 import Image from "next/future/image";
-import { PosterLoader } from "../../PosterLoader";
-import { Navbar } from "../../components/Navbar";
+import { PosterLoader } from "../../../PosterLoader";
+import { Navbar } from "../../../components/Navbar";
 import moment from "moment";
-import fetcher from "../../Fetcher";
-import { CreditsResponse } from "../../types/GetCreditsTypes";
-import { Cast as CastWrapper } from "../../types/Cast";
+import fetcher from "../../../Fetcher";
+import { CreditsResponse } from "../../../types/GetCreditsTypes";
 import Placeholder from "../../assets/MovieSVG.svg";
 import Link from "next/link";
 import { Fragment, useEffect, useState } from "react";
-
+import { CastWidget } from "../../../components/CastWidget";
+import { Cast } from "../../../types/Cast";
+import { isMovieResult } from "../../../types/MultiSearchTypes";
 
 //TODO: Add case for when The movie is not released yet
 //TODO: Add placeholder image for movie poster
-export default function MoviePage({ data }: { data: Movie }) {
+export default function MoviePage({ data, mediaType }: { data: Movie, mediaType: string }) {
     console.log(data);
     return (
         <div>
@@ -70,7 +71,7 @@ export default function MoviePage({ data }: { data: Movie }) {
                 </div>
                 <br />
 
-                <CastWidget id={data.id} />
+                <CastWidget id={data.id} mediaType={mediaType} />
             </div>
 
         </div>
@@ -98,113 +99,7 @@ const Metrics = ({ data, styles }: { data: Movie, styles: string }) => {
     )
 }
 
-const CastWidget = ({ id }: { id: number }) => {
-    return (
-        <div>
-            <p className="font-semibold text-2xl text-neutral-100 mb-3">Actors</p>
-            <CastWrapper id={id} />
-        </div>
-    )
-}
 
-//TODO: Build "Show More" Button search
-// pass cast data and movie id as props
-const CastWrapper = ({ id }: { id: number }) => {
-
-    const { data, error }: SWRResponse<CreditsResponse, Error> = useSWR(`/api/getmoviecredits/${id}`, fetcher);
-    // console.log(data);
-
-
-    //TODO: Replace placeholder.com with something better
-    if (!data && !error) return <ActorSkeletons />;
-    if (!data) return <Error />;
-    return (
-        <div className="flex flex-row overflow-x-auto md:scrollbar-thin md:scrollbar-track-gray-100 md:scrollbar-thumb-red-600 pb-5 md:ml-2 md:mr-2">
-            <CastContent data={data} />
-        </div>
-    )
-}
-
-const CastContent = ({ data }: { data: CreditsResponse }) => {
-    const [showMore, setShowMore] = useState(false);
-
-    useEffect(() => {
-        if (data.cast.length > 10) setShowMore(true);
-
-    }, [])
-
-
-    return (
-        <Fragment>
-            {data.cast.map((cast: CastWrapper, index: number) => {
-                if (index <= 10) return (
-                    <Link key={cast.id} href={`/person/${cast.id}`} passHref>
-                        <a>
-                            <div className="grid auto-cols-max ml-1 mr-1 p-2 hover:bg-neutral-900 rounded-sm transition-all delay-50">
-                                <Image
-                                    src={cast.profile_path ? cast.profile_path : Placeholder.src}
-                                    alt={`Image of ${cast.name}`}
-                                    loader={PosterLoader}
-                                    width={125}
-                                    height={187}
-                                    className="rounded-md w-[125px] h-[187px]"
-                                />
-                                <div className="w-[125px] truncate overflow-x-hidden text-neutral-100">
-                                    <p className="truncate">{cast.name}</p>
-                                    <p className="truncate text-neutral-400">{cast.character}</p>
-                                </div>
-                            </div>
-                        </a>
-                    </Link>
-                );
-            })}
-            {
-                showMore ?
-                    <Link href="#" passHref>
-                        <a className="flex items-center justify-center text-neutral-100 rounded-sm font-medium text-lg hover:bg-neutral-900 pl-12 pr-12">
-                            Show more
-                        </a>
-                    </Link>
-                    : <Fragment />
-            }
-        </Fragment>
-    )
-}
-
-const Error = () => {
-    return (
-        <div className="flex flex-col justify-center items-center w-auto h-[252px]">
-            <p className="font-semibold text-3xl text-neutral-100">Something went wrong...</p>
-            <p className="font-semibold text-lg text-neutral-400">Please check your internet connection.</p>
-        </div>
-    )
-}
-
-const ActorSkeletons = () => {
-    return (
-        <div className="flex flex-row overflow-x-auto gap-2">
-            <ActorSkeleton />
-            <ActorSkeleton />
-            <ActorSkeleton />
-            <ActorSkeleton />
-            <ActorSkeleton />
-            <ActorSkeleton />
-            <ActorSkeleton />
-            <ActorSkeleton />
-            <ActorSkeleton />
-            <ActorSkeleton />
-        </div>
-    )
-}
-
-
-const ActorSkeleton = () => (
-    <div className="mb-2">
-        <div className="w-[125px] h-[187px] animate-pulse bg-gray-100 rounded-md mb-2"></div>
-        <div className="w-24 h-1 animate-pulse bg-gray-100 rounded-sm mb-1"></div>
-        <div className="w-20 h-1 animate-pulse bg-gray-200 rounded-sm"></div>
-    </div>
-)
 export async function getServerSideProps(context: GetServerSidePropsContext) {
     let data: Movie;
 
@@ -213,7 +108,8 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
 
     return {
         props: {
-            data: data
+            data: data,
+            mediaType: "movie"
         }
     }
 }
